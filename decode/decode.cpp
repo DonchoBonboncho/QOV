c10112bfae1d0acde2af6a2de97da90e3089d72e
Doncho <donitepavi4arov@gmail.com>
2023-10-31 00:02:11 +0200

    added different typed of encodin, DONT WORK

--- decode/decode.cpp
+++ decode/decode.cpp
@@ -8,6 +8,15 @@
 
 using namespace cv;
 
+#define out(x) #x << " = " << x << "  "
+
+const int modHash = 32;
+
+int hashTable[modHash];
+int valHashR[modHash];
+int valHashG[modHash];
+int valHashB[modHash];
+
 int main(){
 
 #ifdef TIME
@@ -24,29 +33,89 @@ int main(){
 	fin.read((char *)&currH, sizeof( currH ) );
 	fin.read((char *)&currW, sizeof( currW ) );
 
-	//std::cerr << " ! " << currH << " " << currW << std::endl;
+	std::cerr << " ! " << currH << " " << currW << std::endl;
 
-	std::cout << "P3" << std::endl;
-	std::cout << currW << " " << currH << " 255 " << std::endl;
+	//std::cout << "P3" << std::endl;
+	//std::cout << currW << " " << currH << " 255 " << std::endl;
 
+	int prevR, prevB, prevG;
+	int runNum = 0;
+	int numHash = 1;
 	for( int i=0 ; i < currH ; i++ ){
 		for( int j=0 ; j < currW ; j++ ){
 
-			int type;
-			fin.read( (char *) &type, sizeof( type ) );
-
-
-			if( type == 9 ){
-
-				int currR;
-				fin.read( (char * )&currR, sizeof( currR ) );
-				int currG;
-				fin.read( (char * )&currG, sizeof( currG ) );
-				int currB;
-				fin.read( (char * )&currB, sizeof( currB ) );
-
-				std::cout << currR << " " << currG << " " << currB << std::endl; 
+			if( runNum ){
+				//std::cout << prevR << " " << prevG << " " << prevB << std::endl;
+				runNum --;
+				continue;
 			}
+
+			int currR, currB, currG;
+
+			int info;
+			//uint8_t info;
+			fin.read( (char*) &info, sizeof( info ) );
+			int type = info >> 5;
+			std::cout << type << std::endl;
+			//std::cerr << out( type ) << std::endl;
+			if( type == 5 ){
+				runNum = info | ( ( 1 << 6 ) -1 ); // last 5 bits
+				std::cerr << out( runNum ) << std::endl;
+				//std::cout << prevR << " " << prevG << " " << prevB << std::endl;
+				runNum --;
+				continue;
+			}
+			if( type == 1 ){
+				int dx = info | ( ( 1 << 6 ) -1 );
+				dx -= 15;
+				currR = prevR + dx;
+
+				int info2;
+				//uint8_t info2;
+				fin.read( (char*) &info2, sizeof( info2 ) );
+
+				int dg = ( info2 >> 4 );
+				dg -= 7;
+				currG = prevG + dg;
+
+				int db = info2 | ( ( 1 << 5 ) -1 );
+				db -= 7;
+				currB = prevB + db;
+			}
+				
+			if( type == 2 ){
+				int currHashInd;
+				currHashInd =  info | ( ( 1 << 6 ) -1 );
+				currR = valHashR[ currHashInd ];
+				currG = valHashG[ currHashInd ];
+				currB = valHashB[ currHashInd ];
+			}
+
+			if( type == 3 ){
+
+				int _r, _g, _b;
+				//uint8_t _r, _g, _b;
+				fin.read( (char*) &_r, sizeof( _r ) );
+				currR = _r;
+				fin.read( (char*) &_g, sizeof( _g ) );
+				currG = _g;
+				fin.read( (char*) &_b, sizeof( _b ) );
+				currB = _b;
+
+				int currHash = ( currR * 3 + currG * 5 + currB * 7 ) % modHash;
+				if( hashTable[currHash] == 0 ){
+					hashTable[currHash] = numHash ++;
+					valHashR[currHash] = currR;
+					valHashG[currHash] = currG;
+					valHashB[currHash] = currB;
+				}
+
+			}
+
+			prevR = currR;
+			prevG = currG;
+			prevB = currB;
+			//std::cout << currR << " " << currG << " " << currB << std::endl; 
 		}
 	}
 
--- encode/encode.cpp
+++ encode/encode.cpp
@@ -26,6 +26,11 @@ using namespace cv;
 const int MAX_FRAME_H = 1080;
 const int MAX_FRAME_W = 1920;
 
+const int modHash = 32;
+
+int hashTable[modHash];
+int numHash = 1;
+
 class Pixel{
 	private:
 
@@ -35,12 +40,23 @@ class Pixel{
 
 	public:
 
+	Pixel( ){
+		r = b = g = 0;
+	}
+  
+  	Pixel( int _r, int _g, int _b ){
+  		r = _r;
+  		g = _g;
+  		b = _b;
+  	}
+
 	void setRGB( int _r, int _g, int _b ){
 		r = _r;
 		g = _g;
 		b = _b;
 	}
 
+	// getters
 	const int& getR( ) const{
 		return r;
 	}
@@ -53,7 +69,16 @@ class Pixel{
 		return b;
 	}
 
+	bool operator ==( const Pixel& currP ){
+		if( r != currP.r ) return false;
+		if( g != currP.g ) return false;
+		if( b != currP.b ) return false;
+		return true;
+	}
+
 };
+
+Pixel valHash[modHash];
 
 class Frame{
 	private:
@@ -75,7 +100,11 @@ class Frame{
 		for( int i=0 ; i < FrameH ; i++ ) pixels[i] = new Pixel [FrameW];
 	}
 
-	void setPixelVal( const int& r, const int& b, const int& g, const int& x, const int& y ){
+	void setPixelPixel( const Pixel& p, const int& x, const int& y ){
+		pixels[x][y] = p;
+	}
+
+	void setPixelRGB( const int& r, const int& b, const int& g, const int& x, const int& y ){
 		pixels[x][y].setRGB( r, g, b );
 	}
 
@@ -99,7 +128,7 @@ int main(){
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 #endif
 
-	std::ofstream fout("../decode/encodeBytes.dat", std::ios::out | std::ios::binary);
+	std::ofstream fout("../build/encodeBytes.dat", std::ios::out | std::ios::binary);
 
 	if(!fout) {
 		std::cout << "Ne moga da otvorq OUTPUT faila" << std::endl;
@@ -128,6 +157,8 @@ int main(){
 	fout.write( (char *)&IMG_W, sizeof( IMG_W ) );
 
 	Frame currFrame( IMG_H, IMG_W );
+	Pixel prev;
+	int cntRun = 0;
 	for(int i=0; i < IMG_H ; i++) {
 		for(int j=0; j < IMG_W; j++) {
 
@@ -135,16 +166,102 @@ int main(){
 			int g = img.at< cv::Vec3b>( i, j )[1];
 			int r = img.at< cv::Vec3b>( i, j )[2];
 
-			currFrame.setPixelVal( r, g, b, i, j );
-			
-			//uint8_t type = 
-			//int type = ( 1 << 9 ) -1;
-			std::cerr << out( type ) << std::endl;
-			fout.write( (char*) &type, sizeof( type ) );
-			fout.write( (char*) &r, sizeof( r ) );
-			fout.write( (char*) &g, sizeof( g ) );
-			fout.write( (char*) &b, sizeof( b ) );
+			Pixel curr( r, g, b );
 
+			currFrame.setPixelPixel( curr, i, j );
+
+
+			//run operation
+			// 0 0 0 | _ _ _ _ _
+			if( ( i || j ) and ( i != IMG_H and j != IMG_W ) and curr == prev ){
+				cntRun ++;
+				if( cntRun < ( 1 << 6 )-1 ) continue;
+			}
+			if( cntRun ){
+				int type = 5;
+				int info;
+				//uint8_t info;
+				info = ( type << 5 );
+				info |= cntRun;
+				std::cerr << out( cntRun ) << std::endl;
+				cntRun = 0;
+				fout.write( (char*) &info, sizeof( info ) );
+				std::cout << 0 << std::endl;
+
+				if( curr == prev ) continue;
+			}
+
+			int dr = prev.getR() - curr.getR();
+			int dg = prev.getG() - curr.getG();
+			int db = prev.getB() - curr.getB();
+			if( dr >= -15 and dr <= 16 && dg >= -7 and dg <= 8 && db >= -7 and db <= 8 ){
+				// diff
+				// 0 0 1 | _ _ _ _ _  . _ _ _ _ | _ _ _ _
+				int type = 1;
+				int info1 = 0;
+				//uint8_t info1 = 0;
+				info1 = ( type << 5 );
+				dr += 15;
+				info1 |= dr;
+
+				int info2 = 0;
+				//uint8_t info2 = 0;
+				dg += 7;
+				info2 = ( dg << 4 );
+
+				db += 7;
+				info2 |= db;
+
+				fout.write( (char * ) &info1, sizeof( info1 ) );
+				fout.write( (char * ) &info2, sizeof( info2 ) );
+
+				std::cout << 1 << std::endl;
+				prev = curr;
+				continue;
+			}
+
+			// hash
+			// 0 1 0 | _ _ _ _ _
+			int currHash = ( curr.getG() * 3 + curr.getG() * 5 + curr.getR() * 7 ) & modHash;
+			if( hashTable[currHash] != 0 and valHash[currHash] == curr ){
+				int currInd = hashTable[currHash];
+				int type = 2;
+
+				int info = 0;
+				//uint8_t info = 0;
+				info  = ( type << 5 );
+				info |= currInd;
+
+				fout.write( (char * ) & info, sizeof( info ) );
+				prev = curr;
+
+				std::cout << 2 << std::endl;
+				continue;
+			}
+
+
+			int type = 3;
+			//uint8_t infoType = ( type << 5 );
+			//uint8_t infoR = curr.getR();
+			//uint8_t infoG = curr.getG();
+			//uint8_t infoB = curr.getB();
+			int infoType = ( type << 5 );
+			int infoR = curr.getR();
+			int infoG = curr.getG();
+			int infoB = curr.getB();
+			
+			fout.write( (char *) &infoType, sizeof( infoType ) );
+			fout.write( (char *) &infoR, sizeof( infoR ) );
+			fout.write( (char *) &infoG, sizeof( infoG ) );
+			fout.write( (char *) &infoB, sizeof( infoB ) );
+
+			if( numHash < modHash ){
+				hashTable[ currHash ] = numHash ++;
+				valHash[ currHash ] = curr;
+			}
+
+			std::cout << 3 << std::endl;
+			prev = curr;
 		}
 	}
 
